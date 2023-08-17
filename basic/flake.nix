950016c685aac2568392a409c43b9e136d7cce7c
Filippo Berto <berto.f@protonmail.com>
2023-08-17 23:10:20 +0200

    Better python and basic templates

--- basic/flake.nix
+++ basic/flake.nix
@@ -8,53 +8,51 @@
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = inputs@{ flake-parts, systems, pre-commit-hooks-nix, ... }:
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    imports = [
+      # To import a flake module
+      # 1. Add foo to inputs
+      # 2. Add foo as a parameter to the outputs function
+      # 3. Add here: foo.flakeModule
+      inputs.pre-commit-hooks-nix.flakeModule
+    ];
+    perSystem =
+      { config
+        # , self'
+        # , inputs'
+      , pkgs
+        # , system
+      , ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
 
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-        pre-commit-hooks-nix.flakeModule
-      ];
-      perSystem =
-        { config
-          # , self'
-          # , inputs'
-        , pkgs
-          # , system
-        , ...
-        }: {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
+        # # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+        # _module.args.pkgs = import nixpkgs {
+        #   inherit system;
+        #   config.allowUnfree = true;
+        # };
 
-          # # This sets `pkgs` to a nixpkgs with allowUnfree option set.
-          # _module.args.pkgs = import nixpkgs {
-          #   inherit system;
-          #   config.allowUnfree = true;
-          # };
-
-          pre-commit.settings.hooks = {
-            deadnix.enable = true;
-            nixpkgs-fmt.enable = true;
-            statix.enable = true;
-          };
-
-          devShells.default = pkgs.mkShell {
-            shellHook = ''
-              ${config.pre-commit.installationScript}
-            '';
-          };
-
-          formatter = pkgs.nixpkgs-fmt;
+        pre-commit.settings.hooks = {
+          deadnix.enable = true;
+          nixpkgs-fmt.enable = true;
+          statix.enable = true;
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
       };
+    flake = {
+      # The usual flake attributes can be defined here, including system-
+      # agnostic ones like nixosModule and system-enumerating ones, although
+      # those are more easily expressed in perSystem.
     };
+  };
 }
--- python/flake.nix
+++ python/flake.nix
@@ -2,62 +2,79 @@
   description = "A very basic flake";
 
   inputs = {
+    systems.url = "github:nix-systems/default";
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        myPython = pkgs.python3;
-        myPackages = myPython.pkgs;
-      in
-      {
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = builtins.path { path = ./.; name = "flake-templates-src"; };
-            hooks = {
-              deadnix.enable = true;
-              nixpkgs-fmt.enable = true;
-              statix.enable = true;
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    imports = [
+      # To import a flake module
+      # 1. Add foo to inputs
+      # 2. Add foo as a parameter to the outputs function
+      # 3. Add here: foo.flakeModule
+      inputs.pre-commit-hooks-nix.flakeModule
+    ];
+    perSystem =
+      { config
+        # , self'
+        # , inputs'
+      , pkgs
+        # , system
+      , ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
 
-              autoflake.enable = true;
-              black.enable = true;
-            };
-          };
+        # # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+        # _module.args.pkgs = import nixpkgs {
+        #   inherit system;
+        #   config.allowUnfree = true;
+        # };
+
+        pre-commit.settings.hooks = {
+          deadnix.enable = true;
+          nixpkgs-fmt.enable = true;
+          statix.enable = true;
+
+          autoflake.enable = true;
+          black.enable = true;
         };
 
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           name = "impurePythonEnv";
           venvDir = ".venv";
           buildInputs = [
-            myPython
-            myPackages.venvShellHook
+            pkgs.python3
+            pkgs.python3Packages.venvShellHook
 
-            myPackages.virtualenv
-            myPackages.ipython
-            (myPackages.matplotlib.override { enableTk = true; })
-            myPackages.pandas
-            myPackages.seaborn
+            # pkgs.python3Packages.ipython
+            # (pkgs.python3Packages.matplotlib.override { enableTk = true; })
+            # pkgs.python3Packages.pandas
+            # pkgs.python3Packages.seaborn
           ];
 
           postVenvCreation = ''
             unset SOURCE_DATE_EPOCH
-            pip install -r requirements.txt
+            find . -name requirements.txt -exec pip install -r {} \;
           '';
 
           postShellHook = ''
             # allow pip to install wheels
             unset SOURCE_DATE_EPOCH
-            ${self.checks.${system}.pre-commit-check.shellHook}
+            ${config.pre-commit.installationScript}
           '';
         };
 
         formatter = pkgs.nixpkgs-fmt;
-      });
+      };
+    flake = {
+      # The usual flake attributes can be defined here, including system-
+      # agnostic ones like nixosModule and system-enumerating ones, although
+      # those are more easily expressed in perSystem.
+    };
+  };
 }
